68e53d610c74c135bbafad9ca292228525add963
stanleymcc <stan_mcc47@hotmail.com>
2015-04-18 16:56:34 -0400

    library

---
+++ client/cloud_library.c
@@ -0,0 +1,17 @@
+int mycloud_putfile(char *MachineName, int TCPport, int SecretKey, char *Filename, char *data, int datalen)
+{
+
+}
+
+int mycloud_getfile(char *MachineName, int TCPport, int SecretKey, char *Filename, char *data, int datalen)
+{
+
+}
+int mycloud_delfile(char *MachineName, int TCPport, int SecretKey, char *Filename)
+{
+
+}
+int mycloud_listfiles(char *MachineName, int TCPport, int SecretKey, char *listbuf, int listbuflen)
+{
+    
+}
