19ef84d3cbbda0d0979b634486b5e746f4f14203
stanleymcc <stan_mcc47@hotmail.com>
2015-04-21 12:22:54 -0400

    started getfile

--- client/cloud_library.c
+++ client/cloud_library.c
@@ -1,8 +1,7 @@
 #include "csapp.h"
 #define NAME_SIZE 80
 #define FILE_SIZE 100000
-#define REQUEST_SIZE 4 
-
+#define REQUEST_SIZE 4
 #define DNS_SIZE 40
 int mycloud_putfile(char *MachineName, int TCPport, int SecretKey, char *Filename, char *data, int datalen)
 {
@@ -14,6 +13,8 @@ int mycloud_putfile(char *MachineName, int TCPport, int SecretKey, char *Filenam
 	messageSize = key_size +  strlen(Filename) + FILE_SIZE + NAME_SIZE + datalen;  //request type size
 
 	message = (char*) malloc(sizeof(char)*messageSize); 
+
+
 	if(message == NULL)
 	{
 		printf(stderr, "Memory Error -> mcputs\n"); 
@@ -39,82 +40,57 @@ int mycloud_putfile(char *MachineName, int TCPport, int SecretKey, char *Filenam
 
 
 	size_t num; 
-	char status_buf[4];
+	char status_buf[REQUEST_SIZE];
 	unsigned int status; 
 	rio_t rio; 
 	Rio_readinitb(&rio,clientfd); 
 
 	status = -1; 
-	if(n = Rio_readnb(&rio,status_buf,4) == 4)
+	if(n = Rio_readnb(&rio,status_buf,REQUEST_SIZE) == REQUEST_SIZE)
 	{
-		memcpy(&netOrder,&status_buf,4);
+		memcpy(&netOrder,&status_buf,REQUEST_SIZE);
 		status = ntohl(netOrder); 
 	}
 	Close(clientfd);
 	return status; 
 	
-
-
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*int mycloud_getfile(char *MachineName, int TCPport, int SecretKey, char *Filename, char *data, int datalen)
+int mycloud_getfile(char *MachineName, int TCPport, int SecretKey, char *Filename, char *data, int datalen)
 {
+	int clientfd; 
+	char *message;
+	unsigned int messageSize, netOrder; 
+	int key_size = (SecretKey == 0 ? 1 : (int)(log10(SecretKey)+1));
+	messageSize =  key_size + REQUEST_SIZE + NAME_SIZE;
 
+
+	message = (char *)malloc(sizeof(char)*message_buf); 
+
+	if(message == NULL)
+	{
+		printf(stderr,"Memory Error - mcgets\n"); 
+	}
+	char *message_buf = message; 
+
+	netOrder = htonl(SecretKey);
+	memcpy(message_buf,&netOrder,key_size); 
+	message_buf += key_size; 
+
+	unsigned int request = 0; 
+	netOrder = htonl(request); 
+	memcpy(message_buf, &netOrder, NAME_SIZE); 
+	message_buf +=  REQUEST_SIZE; 
+
+	memcpy(message_buf,Filename,NAME_SIZE);
+	message_buf += NAME_SIZE; 
+
+	clientfd = Open_clientfd(MachineName,TCPport); 
+	Rio_writen(clientfd,message,messageSize); 
+	free(message); 
+
+	size_t num; 
+	int STATUS_SIZE = 
 }
 int mycloud_delfile(char *MachineName, int TCPport, int SecretKey, char *Filename)
 {
@@ -123,4 +99,4 @@ int mycloud_delfile(char *MachineName, int TCPport, int SecretKey, char *Filenam
 int mycloud_listfiles(char *MachineName, int TCPport, int SecretKey, char *listbuf, int listbuflen)
 {
     
-}*/
+}
