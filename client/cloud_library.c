5d3dfc884983372ec67fee9121d2b357d7b2a0ee
stanleymcc <stan_mcc47@hotmail.com>
2015-04-23 14:50:15 -0400

    library done ~~~Not Tested ~~~~

--- client/cloud_library.c
+++ client/cloud_library.c
@@ -47,7 +47,7 @@ int mycloud_putfile(char *MachineName, int TCPport, int SecretKey, char *Filenam
 	Rio_readinitb(&rio,clientfd); 
 
 	status = -1; 
-	if(n = Rio_readnb(&rio,status_buf,REQUEST_SIZE) == REQUEST_SIZE)
+	if(num = Rio_readnb(&rio,status_buf,REQUEST_SIZE) == REQUEST_SIZE)
 	{
 		memcpy(&netOrder,&status_buf,REQUEST_SIZE);
 		status = ntohl(netOrder); 
@@ -91,15 +91,164 @@ int mycloud_getfile(char *MachineName, int TCPport, int SecretKey, char *Filenam
 	free(message); 
 
 	size_t num; 
-	int STATUS_SIZE = REQUEST_SIZE + FILE_SIZE + 4; 
+	int currStat = 4; 
+	int NEW_STATUS_SIZE = REQUEST_SIZE + FILE_SIZE + currStat; 
 	char buffer[STATUS_SIZE]; 
-	
+	char file_buf[4];
+	char file_data[FILE_SIZE]; 
+	unsigned int stat; 
+	unsigned int fileSize = *datalen; 
+	rio_t rio; 
+
+	Rio_readinitb(&rio,clientfd); 
+
+	if((num = Rio_readnb(&rio,buffer,currStat)) == currStat)
+	{
+		memcpy(&netOrder,&buffer,currStat);
+		stat = ntohl(netOrder); 
+	} 
+	else
+	{
+		stat = -1; 
+	}
+
+	if((num = Rio_readnb(&rio,file_data,fileSize)) == fileSize)
+	{
+		*data = (char*) malloc (sizeof(char)*fileSize); 
+		if(*data == NULL)
+		{
+			fprintf(stdeff, "Memory Error ~ mcget\n");
+			return -1; 
+		}
+	}
+
+	else
+	{
+		status = -1; 
+	}
+
+	Close(clientfd); 
+	return stat; 
+
 }
 int mycloud_delfile(char *MachineName, int TCPport, int SecretKey, char *Filename)
 {
+	int clientfd; 
+	char *message;
+	unsigned int messageSize, netOrder; 
+	int key_size = (SecretKey == 0 ? 1 : (int)(log10(SecretKey)+1));
+	messageSize =  key_size + REQUEST_SIZE + NAME_SIZE;
+	message = (char *) malloc(sizeof(char)*message_buf); 
+
+	if(message == NULL)
+	{
+		printf(stderr,"Memory Error ~ // mcdel\n"); 
+	}
+	char *message_buf = message; 
+
+	netOrder = htonl(SecretKey);
+	memcpy(message_buf,&netOrder,key_size); 
+	message_buf += key_size; 
+
+	unsigned int request = 2; 
+	netOrder = htonl(request);
+	memcpy(message_buf,&netOrder,REQUEST_SIZE); 
+	message_buf += REQUEST_SIZE; 
+
+	memcpy(message_buf,Filename,NAME_SIZE);
+	message_buf += NAME_SIZE; 
+
+	clientfd = Open_clientfd(MachineName,TCPport); 
+	Rio_writen(clientfd,message,messageSize); 
+	free(message); 
+
+	size_t num; 
+	int currStat = 4;
+	char buffer[STATUS_SIZE]; 
+	unsigned int stat; 
+	rio_t rio; 
+
+	Rio_readinitb(&rio,clientfd); 
+	stat = -1; 
+	if((num = Rio_readnb(&rio,buffer,currStat)) == currStat)
+	{
+		memcpy(&netOrder,&buffer,currStat);
+		stat = ntohl(netOrder); 
+	} 
+	Close(clientfd);
+	return stat; 
 
 }
-int mycloud_listfiles(char *MachineName, int TCPport, int SecretKey, char *listbuf, int listbuflen)
+int mycloud_listfiles(char *MachineName, int TCPport, int SecretKey, char **listbuf, int *listbuflen)
 {
-    
+ 	int clientfd; 
+	char *message;
+	unsigned int messageSize, netOrder; 
+	int key_size = (SecretKey == 0 ? 1 : (int)(log10(SecretKey)+1));
+	messageSize =  key_size + REQUEST_SIZE + NAME_SIZE;
+	message = (char *) malloc(sizeof(char)*message_buf); 
+
+	if(message == NULL)
+		{
+			printf(stderr,"Memory Error ~ // list files\n"); 
+		}
+	char *message_buf = message; 
+
+	netOrder = htonl(SecretKey);
+	memcpy(message_buf,&netOrder,key_size); 
+	message_buf += key_size; 
+
+	unsigned int request = 3; 
+	netOrder = htonl(request);
+	memcpy(message_buf,&netOrder,REQUEST_SIZE); 
+	message_buf += REQUEST_SIZE;    
+
+	clientfd = Open_clientfd(MachineName,TCPport); 
+	Rio_writen(clientfd,message,messageSize); 
+	free(message); 
+
+	size_t num; 
+	int currStat = 4;
+	char Sat_Buffer[STATUS_SIZE];
+	char listsize[4]; 
+	char list[FILE_SIZE]; 
+	unsigned int stat; 
+	rio_t rio; 
+
+	Rio_readinitb(&rio,clientfd); 
+	int max = 4; 
+
+	if((num = Rio_readnb(&rio,buffer,currStat, )) == currStat)
+	{
+		memcpy(&netOrder,&buffer,currStat);
+		stat = ntohl(netOrder); 
+	} 
+	else
+	{
+		stat = -1; 
+	}
+
+	if((num = Rio_readnb(&rio,listsize,max)) == max)
+	{
+		 memcpy(*netOrder,&listsize,max); 
+		 listbuflen = ntohl(netOrder); 
+	}
+	else
+	{
+		stat = -1; 
+	}
+	if((num= Rio_readnb(&rio,listbuf,*listbuflen)) == *listbuflen)
+	{
+		*listbuf = (char*) malloc (*listbuflen);
+		stat = 0; 
+	}
+	else
+	{
+		stat = -1; 
+	}
+
+	Close(clientfd);
+	return stat; 
+
+
 }
